a851ab90d1f66141eb83b4e80b12c553c588e58e
Piotr Migdal <Piotr.Migdal@PARSEQ.COM>
2021-09-16 11:56:44 +0100

    Alignend Breeze layout. Ready to do login and about frontend

---
+++ resources/views/timeline/index.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight uppercase">
+            {{ __('Timeline ' . Auth::user()->name) }}
+        </h2>
+    </x-slot>
+    test
+</x-layouts.app>
+
