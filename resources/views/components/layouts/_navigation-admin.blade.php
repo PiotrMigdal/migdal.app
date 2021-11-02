39c589aa1d3e9c477d29c80e763e75c8ddbd3371
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-11-02 00:03:53 +0000

    Changed top menu links

--- resources/views/components/layouts/_navigation-admin.blade.php
+++ resources/views/components/layouts/_navigation-admin.blade.php
@@ -3,19 +3,19 @@
 <aside class="hidden ml-5 rounded-xl lg:block w-52 lg:flex-shrink-0 mt-1">
     <nav class="rounded-b-xl bg-brand-gray-dark h-full space-y-2 pt-4">
         <x-nav-left-link :href="route('user.edit')" :active="request()->routeIs('user.edit')">
-            {{ __('Profile') }}
+            {{ __('Personal info') }}
         </x-nav-left-link>
         <x-nav-left-link :href="route('about.index.admin')" :active="request()->routeIs(['about.index.admin', 'about.edit', 'about.create'])">
-            {{ __('About') }}
+            {{ __('About articles') }}
         </x-nav-left-link>
         <x-nav-left-link :href="route('jobs.index.admin')" :active="request()->routeIs(['jobs.index.admin', 'jobs.edit', 'jobs.create'])">
             {{ __('Job history') }}
         </x-nav-left-link>
         <x-nav-left-link :href="route('projects.index.admin')" :active="request()->routeIs(['projects.index.admin', 'projects.edit', 'projects.create'])">
-            {{ __('Projects') }}
+            {{ __('Your projects') }}
         </x-nav-left-link>
         <x-nav-left-link :href="route('courses.index.admin')" :active="request()->routeIs(['courses.index.admin', 'courses.edit', 'courses.create'])">
-            {{ __('Courses') }}
+            {{ __('Your courses') }}
         </x-nav-left-link>
     </nav>
 </aside>
@@ -23,18 +23,18 @@
 <!-- Responsive left navigation -->
 <nav class="lg:hidden grid grid-cols-2 gap-2 my-4">
         <x-responsive-nav-left-link :href="route('user.edit')" :active="request()->routeIs('user.edit')">
-            {{ __('Profile') }}
+            {{ __('Personal info') }}
         </x-responsive-nav-left-link>
         <x-responsive-nav-left-link :href="route('about.index.admin')" :active="request()->routeIs(['about.index.admin', 'about.edit', 'about.create'])">
-            {{ __('About') }}
+            {{ __('About articles') }}
         </x-responsive-nav-left-link>
         <x-responsive-nav-left-link :href="route('jobs.index.admin')" :active="request()->routeIs(['jobs.index.admin', 'jobs.edit', 'jobs.create'])">
             {{ __('Job history') }}
         </x-responsive-nav-left-link>
         <x-responsive-nav-left-link :href="route('projects.index.admin')" :active="request()->routeIs(['projects.index.admin', 'projects.edit', 'projects.create'])">
-            {{ __('Projects') }}
+            {{ __('Your projects') }}
         </x-responsive-nav-left-link>
         <x-responsive-nav-left-link :href="route('courses.index.admin')" :active="request()->routeIs(['courses.index.admin', 'courses.edit', 'courses.create'])">
-            {{ __('Courses') }}
+            {{ __('Your courses') }}
         </x-responsive-nav-left-link>
 </nav>
