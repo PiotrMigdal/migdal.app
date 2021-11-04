5998cff24a5cb3612c9e04f2bbe98fb18e534683
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-11-04 22:42:41 +0000

    Changed Enlarged image in show

--- resources/views/components/article-image-card.blade.php
+++ resources/views/components/article-image-card.blade.php
@@ -1,13 +1,15 @@
 
 <article {{ $attributes->merge(['class' => 'card-shadow bg-brand-gray-dark mb-6']) }}>
-    @isset($featured_image)
-        {{ $featured_image }}
-    @endisset
     <div class="p-4 lg:p-8">
         <div class="lg:flex">
             @isset($thumbnail)
             <aside class="m-auto lg:flex-shrink-0 p-6">
                 {{ $thumbnail }}
+            </aside>
+            @endisset
+            @isset($featured_image)
+            <aside class="m-auto lg:flex-shrink-0 p-6">
+                {{ $featured_image }}
             </aside>
             @endisset
             <div class="lg:flex-1 px-4">
--- resources/views/components/image-enlarge.blade.php
+++ resources/views/components/image-enlarge.blade.php
@@ -1,6 +1,6 @@
 @props(["filename"])
 
-<img {{ $attributes->merge(['class' => 'enlarge m-auto rounded-xl border-gray-50 border-2 object-cover cursor-pointer hover:border-brand-pink hover:opacity-90']) }} title="Click to enlarge" src="{{ asset('storage/' . $filename) }}">
+<img {{ $attributes->merge(['class' => 'enlarge m-auto rounded-md border-gray-50 border-2 object-cover cursor-pointer hover:border-brand-pink hover:opacity-90 w-72']) }} title="Click to enlarge" src="{{ asset('storage/' . $filename) }}">
 
 <div class='enlarged top-0 left-0 w-screen h-screen fixed bg-black bg-opacity-80 hidden cursor-pointer'>
     <img class="m-auto object-cover" src="{{ asset('storage/' . $filename) }}">
