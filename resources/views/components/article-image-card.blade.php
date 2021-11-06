e62d7df148201e449476929c5984ef83ea9652b0
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-11-06 00:10:29 +0000

    Added border to article

--- resources/views/components/article-image-card.blade.php
+++ resources/views/components/article-image-card.blade.php
@@ -1,7 +1,7 @@
 
 <article {{ $attributes->merge(['class' => 'card-shadow bg-brand-gray-dark mb-6 article-content']) }}>
     <div class="p-4 lg:p-8">
-        <div class="lg:flex">
+        <div class="lg:flex border-b border-gray-600 mb-2 pb-2">
             @isset($thumbnail)
             <aside class="m-auto lg:flex-shrink-0 p-6">
                 {{ $thumbnail }}
