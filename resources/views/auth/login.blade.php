63ff0a0b34cb337d63aa9480c65827b3dbbb7ae8
PiotrMigdal <PiotrMigdal@users.noreply.github.com>
2021-11-09 00:19:46 +0000

    Added registration link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,8 +42,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-300 hover:text-gray-200" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                    <a class="underline text-sm text-gray-300 hover:text-gray-200" href="{{ route('register') }}">
+                        {{ __('Not registered yet?') }}
                     </a>
                 @endif
 
